5f8e5bc491c4e02da7ff7c1c64d8edb7c99366c6
jordanss1 <101638406+jordanss1@users.noreply.github.com>
2024-05-06 17:56:24 +0100

    channel header dynamic - new icons - data file updated to .ts

--- src/components/servers/Server.tsx
+++ src/components/servers/Server.tsx
@@ -1,6 +1,6 @@
-import { ReactElement } from "react";
+import { ReactElement, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
-import data from "../../../data.json";
+import { ChannelType, data } from "../../../data";
 import * as Icons from "../icons/icons";
 
 type ServerPropsType = {
@@ -8,6 +8,23 @@ type ServerPropsType = {
 };
 
 const Server = ({ name }: ServerPropsType): ReactElement => {
+  const [closedCategories, setClosedCategories] = useState<number[]>([]);
+  const { state } = useLocation();
+
+  let server = data[state ? (state.server as keyof typeof data) : 1];
+  let channel = server.categories
+    .map((c) => c.channels)
+    .flat()
+    .find((c) => (state.channel ? c.id === state.channel : 1)) as ChannelType;
+
+  const toggleCategory = (categoryId: number) => {
+    setClosedCategories((closedCategories) =>
+      closedCategories.includes(categoryId)
+        ? closedCategories.filter((id) => id !== categoryId)
+        : [...closedCategories, categoryId]
+    );
+  };
+
   return (
     <>
       <div className="bg-gray-800 flex flex-col w-60">
@@ -20,30 +37,84 @@ const Server = ({ name }: ServerPropsType): ReactElement => {
           <Icons.Chevron className="ml-auto w-[18px] h-[18px] opacity-80" />
         </button>
         <div className="flex-1 space-y-[21px] pt-3 text-gray-300 w-full overflow-y-scroll font-medium">
-          {data["1"].categories.map(({ id, label, channels }) => (
-            <div key={id} className="px-0.5">
-              {label && (
-                <button className="flex w-full items-center gap-0.5 tracking-wide hover:text-gray-100 text-xs font-title uppercase">
-                  <Icons.Arrow className="w-3 h-3" />
-                  {label}
-                </button>
-              )}
+          {data[state.server as keyof typeof data].categories.map(
+            ({ id, label, channels }) => {
+              const channelList = channels as ChannelType[];
 
-              {channels.map((channel) => {
-                channel = channel as ChannelLinkPropsType["channel"];
+              return (
+                <div key={id} className="px-0.5">
+                  {label && (
+                    <button
+                      onClick={() => toggleCategory(id)}
+                      className="flex w-full items-center gap-0.5 tracking-wide hover:text-gray-100 text-xs font-title uppercase"
+                    >
+                      <Icons.Arrow
+                        className={`${
+                          closedCategories.includes(id) ? "-rotate-90" : ""
+                        } w-3 h-3 transition duration-200`}
+                      />
+                      {label}
+                    </button>
+                  )}
 
-                return (
-                  <div key={channel.id} className="space-y-0.5 mt-[5px]">
-                    <ChannelLink channel={channel} />
-                  </div>
-                );
-              })}
-            </div>
-          ))}
+                  {channelList.map((channel) => {
+                    return (
+                      <div
+                        key={channel.id}
+                        className={` ${
+                          closedCategories.includes(id) && channel.unread
+                            ? "hidden"
+                            : ""
+                        } space-y-0.5 mt-[5px]`}
+                      >
+                        <ChannelLink {...channel} />
+                      </div>
+                    );
+                  })}
+                </div>
+              );
+            }
+          )}
         </div>
       </div>
-      <div className="bg-gray-700 flex flex-col flex-1">
-        <div className="p-3 h-12 flex items-center shadow-sm">General</div>
+      <div className="bg-gray-700 flex flex-shrink min-w-0 flex-col flex-1">
+        <div className="flex h-12 px-2 items-center shadow-sm">
+          <div className="flex items-center">
+            <Icons.Hashtag className="mx-2 h-6 w-6 font-semibold text-gray-400" />
+            <span className="font-title mr-2 text-white">{channel?.label}</span>
+          </div>
+
+          {channel?.description && (
+            <>
+              <div className="w-px h-6 bg-white/[.06] mx-2" />
+              <div className="text-sm truncate font-normal text-gray-200 mx-2">
+                {channel.description}
+              </div>
+            </>
+          )}
+
+          <div className="ml-auto flex items-center">
+            <button className="text-gray-200 hover:text-gray-100">
+              <Icons.HashtagWithSpeechBubble className="mx-2 h-6 w-6" />
+            </button>
+            <button className="text-gray-200 hover:text-gray-100">
+              <Icons.Bell className="mx-2 h-6 w-6" />
+            </button>
+            <button className="text-gray-200 hover:text-gray-100">
+              <Icons.Pin className="mx-2 h-6 w-6" />
+            </button>
+            <button className="text-gray-200 hover:text-gray-100">
+              <Icons.People className="mx-2 h-6 w-6" />
+            </button>
+            <button className="text-gray-200 hover:text-gray-100">
+              <Icons.Inbox className="mx-2 h-6 w-6" />
+            </button>
+            <button className="text-gray-200 hover:text-gray-100">
+              <Icons.QuestionCircle className="mx-2 h-6 w-6" />
+            </button>
+          </div>
+        </div>
+
         <div className="p-3 flex-1 ml-[.1rem] overflow-y-scroll space-y-4">
           {[...Array(40)].map((_, i) => (
             <p key={i}>
@@ -61,19 +132,13 @@ const Server = ({ name }: ServerPropsType): ReactElement => {
 
 export default Server;
 
-type ChannelLinkPropsType = {
-  channel: {
-    id: number;
-    label: string;
-    unread: boolean;
-    icon?: keyof typeof Icons;
-  };
-};
-
-const ChannelLink = ({ channel }: ChannelLinkPropsType): ReactElement => {
+const ChannelLink = ({
+  icon,
+  id,
+  unread,
+  label,
+}: ChannelType): ReactElement => {
   const { state } = useLocation();
-
-  const { label, icon, id } = channel;
 
   let Icon = icon ? Icons[icon] : Icons.Hashtag;
 
@@ -81,7 +146,7 @@ const ChannelLink = ({ channel }: ChannelLinkPropsType): ReactElement => {
 
   const channelState = active
     ? "active"
-    : channel.unread
+    : unread
     ? "inactiveUnread"
     : "inactiveRead";
 
